f74d2978ae9a7228aafec5ab0b76b53eb9934b9a
wuxuwei <2509165479@qq.com>
2021-12-19 16:21:27 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './app.scss'
+
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'antd/dist/antd.min.css';
+
+ReactDOM.render(
+    <App/>
+  ,
+  document.getElementById('app')
+);
