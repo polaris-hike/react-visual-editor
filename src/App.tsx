9e7e487549e5635347af75b291c22d3857b3cee8
wuxuwei <2509165479@qq.com>
2021-12-22 21:45:50 +0800

    feat(editor): 页面主体结构

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,69 @@
 import {useRef, useState} from 'react';
 import './app.scss';
 import { useCallbackRef } from './packages/hooks/useCallbackRef';
+import ReactVisualEditor from './packages/ReactVisualEditor';
 
 function App() {
-  const [pos,setPos] = useState({
-    left: 0,
-    top: 0
-  });
+  // const [pos,setPos] = useState({
+  //   left: 0,
+  //   top: 0
+  // });
 
-  const moveDragger = (()=>{
-    const dragData = useRef({
-      startTop: 0,
-      startLeft: 0,
-      startX: 0,
-      startY: 0,
-    })
+  // const moveDragger = (()=>{
+  //   const dragData = useRef({
+  //     startTop: 0,
+  //     startLeft: 0,
+  //     startX: 0,
+  //     startY: 0,
+  //   })
 
-    const mousedown = useCallbackRef( (e:React.MouseEvent<HTMLDivElement>) => {
-      document.addEventListener('mousemove', mousemove);
-      document.addEventListener('mouseup',mouseup);
-      dragData.current = {
-        startTop: pos.top,
-        startLeft: pos.left,
-        startX: e.clientX,
-        startY: e.clientY
-      }
-    })
+  //   const mousedown = useCallbackRef( (e:React.MouseEvent<HTMLDivElement>) => {
+  //     document.addEventListener('mousemove', mousemove);
+  //     document.addEventListener('mouseup',mouseup);
+  //     dragData.current = {
+  //       startTop: pos.top,
+  //       startLeft: pos.left,
+  //       startX: e.clientX,
+  //       startY: e.clientY
+  //     }
+  //   })
 
-    const mousemove = useCallbackRef((e: MouseEvent) => {
-      const { startLeft,startTop,startX,startY } = dragData.current;
-      const durX = e.clientX - startX;
-      const durY = e.clientY - startY;
-      setPos({
-        left: startLeft + durX,
-        top: startTop + durY,
-      })
-    })
+  //   const mousemove = useCallbackRef((e: MouseEvent) => {
+  //     const { startLeft,startTop,startX,startY } = dragData.current;
+  //     const durX = e.clientX - startX;
+  //     const durY = e.clientY - startY;
+  //     setPos({ 
+  //       left: startLeft + durX,
+  //       top: startTop + durY,
+  //     })
+  //   })
 
-    const mouseup = useCallbackRef((e: MouseEvent) => {
-      document.removeEventListener('mousemove',mousemove);
-      document.removeEventListener('mouseup',mouseup);
-    })
+  //   const mouseup = useCallbackRef((e: MouseEvent) => {
+  //     document.removeEventListener('mousemove',mousemove);
+  //     document.removeEventListener('mouseup',mouseup);
+  //   })
 
 
-    return {mousedown}
-  })()
+  //   return {mousedown}
+  // })()
 
 
   return (
-    <div>
-     <div  style={{
-       width: '50px',
-       height: '50px',
-       position: 'absolute',
-       left:`${pos.left}px`,
-       top:`${pos.top}px`,
-       background: 'black'
-     }}
-     onMouseDown={moveDragger.mousedown}
-     ></div>
-    </div>
+    <>
+    <ReactVisualEditor />
+    </>
+    // <div>
+    //  <div  style={{
+    //    width: '50px',
+    //    height: '50px',
+    //    position: 'absolute',
+    //    left:`${pos.left}px`,
+    //    top:`${pos.top}px`,
+    //    background: 'black'
+    //  }}
+    //  onMouseDown={moveDragger.mousedown}
+    //  ></div>
+    // </div>
   );
 }
 
---
+++ src/packages/ReactVisualEditor.tsx
@@ -0,0 +1,15 @@
+import './ReactVisualEditor.scss';
+
+function ReactVisualEditor() {
+    return (
+        <div className="react-visual-editor">
+            <section className="react-visual-editor-menu">menu</section>
+            <section className="react-visual-editor-head">head</section>
+            <section className="react-visual-editor-operator">operator</section>
+            <section className="react-visual-editor-body">body</section>
+        </div>
+    )
+}
+
+
+export default ReactVisualEditor;
